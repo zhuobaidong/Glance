03b1932c6890bfe3c90f7b62ee343228c91b61e0
DongZhuoBai <dzb1120@163.com>
2025-11-29 17:23:36 +0800

    Add files via upload

---
+++ static/js/imageData.js
@@ -0,0 +1,55 @@
+// imageData.js
+const imageData = [
+  {
+    src: "./static/images/show/General_Object_013.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/General_Object_016.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/qwen/General_Object_002.png",
+    operator: "qwen",
+  },
+  {
+    src: "./static/images/show/Portrait_054.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/General_Object_027.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/Portrait_064.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/qwen/Portrait_039.png",
+    operator: "qwen",
+  },
+  {
+    src: "./static/images/show/General_Object_032.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/qwen/Portrait_094.png",
+    operator: "qwen",
+  },
+  {
+    src: "./static/images/show/General_Object_042.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/Text_Rendering_093.png",
+    operator: "glance",
+  },
+  {
+    src: "./static/images/show/qwen/Text_Rendering_077.png",
+    operator: "qwen",
+  },
+  {
+    src: "./static/images/show/General_Object_057.png",
+    operator: "glance",
+  },
+];
